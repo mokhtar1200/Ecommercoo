adb189d79a5053ac87c1b784e24c0e0d6e82790e
mokhtar1200 <mokhtar.information@gmail.com>
2024-03-31 16:48:42 +0100

    first commit

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,5 +7,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:"skUjxgQr1Tjqa87KBV69q2XzrfAjwNIYjRkTvH2yOskbMAb5Knl4LJKkl5HUIWnVC7hVc0DBFzfgR31knD8zsCcFKWvt7ntSP7KoYfkUm712K6D63ze6VZayrW5FyDOFy6H0WhWimLq7oQ1jRblEZHpKOaIpz2xvD63fVmqq5NksRKodUcdu"
+  // token:"skUjxgQr1Tjqa87KBV69q2XzrfAjwNIYjRkTvH2yOskbMAb5Knl4LJKkl5HUIWnVC7hVc0DBFzfgR31knD8zsCcFKWvt7ntSP7KoYfkUm712K6D63ze6VZayrW5FyDOFy6H0WhWimLq7oQ1jRblEZHpKOaIpz2xvD63fVmqq5NksRKodUcdu"
 })
